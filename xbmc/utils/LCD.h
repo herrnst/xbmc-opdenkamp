2f73a50038a21f46b55cd14ff1c1c8f9e95326ac
Daniel Scheller <d.scheller@gmx.net>
2012-09-25 22:34:53 +0200

    LCD/XLCDproc: Get rid of (obsolete Xbox stuff?) char translation things to not interfere with LCDproc's handling of special/custom chars

--- xbmc/utils/LCD.h
+++ xbmc/utils/LCD.h
@@ -84,7 +84,6 @@ public:
            m_eCurrentCharset(CUSTOM_CHARSET_DEFAULT) {}
 protected:
   virtual void Process() = 0;
-  void StringToLCDCharSet(CStdString& strText, unsigned int iCharsetTab);
   unsigned char GetLCDCharsetCharacter( UINT _nCharacter, int _nCharset=-1);
   void LoadMode(TiXmlNode *node, LCD_MODE mode);
 
